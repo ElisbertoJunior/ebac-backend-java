e5838d79a7b0d65f1b119882cff96178dc196434
Elisberto Junior <elisberto_jr@hotmail.com>
2023-06-28 17:26:46 -0300

    feat: tarefa_wrapper

---
+++ tarefa_wrapper/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 2023;
+        Integer convertedNumber = number;
+
+        System.out.println("Numero convertido: " + convertedNumber);
+
+    }
+}
